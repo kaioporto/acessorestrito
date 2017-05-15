f46f32b778b94eb6e1a9058d44e00ae2888d48f5
Kaio Rodrigo S. Porto <KaioPorto@users.noreply.github.com>
2017-05-15 11:44:27 -0300

    Update index.php

--- views/index.php
+++ views/index.php
@@ -3,22 +3,27 @@
 <body>
 <head>
 	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, intial-scale=1, user-scalable=no">
 	<meta name="author" content="Kaio Rodrigo S. Porto">
-
+	<title>Acesso Restrito</title>
 	<link rel="stylesheet" type="text/css" href="../assets/main.css">
 </head>
 	<header>
-		<ul>
-			<li>Home</li>
-			<li>Photos</li>
-			<li>Contato</li>
-			<li>Agenda</li>
-		</ul>
+		<nav>
+			<ul>
+				<li>Home</li>
+				<li>Photos</li>
+				<li>Contato</li>
+				<li>Agenda</li>
+			</ul>
+		</nav>
 	</header>
 
 	<section>Banda Acesso Restrito</section>
 
-	<footer>Rodapé</footer>
+	<footer>Rodapé
+		<script type="text/javascript" src=""></script>
+	</footer>
 </body>
 </html>
