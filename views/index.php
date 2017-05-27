41ffd94ce6540fb0b4320497cd5783fc2d064a17
KaioPorto <kaio.rodrigo28@gmail.com>
2017-05-27 11:29:55 -0300

    Adicionando gaintime e testando video na index

--- views/index.php
+++ views/index.php
@@ -4,23 +4,39 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="Acesso Restrito é uma banda do">
 	<meta name="viewport" content="width=device-width, intial-scale=1, user-scalable=no">
 	<meta name="author" content="Kaio Rodrigo S. Porto">
 	<title>Acesso Restrito</title>
+	<link rel="stylesheet" type="text/css" href="../bower_components/gaintime/css/gaintime.min.css">
 	<link rel="stylesheet" type="text/css" href="../assets/main.css">
 </head>
-	<header>
+	<header class="gt-top-menu xs-left">
+		<img src="">
+		<label class="menu-toggle"></label>
 		<nav>
 			<ul>
-				<li>Home</li>
-				<li>Photos</li>
-				<li>Contato</li>
-				<li>Agenda</li>
+				<a><li>Home</li></a>
+				<a><li>Photos</li></a>
+				<a><li>Contato</li></a>
+				<a><li>Agenda</li></a>
 			</ul>
 		</nav>
 	</header>
 
-	<section>Banda Acesso Restrito</section>
+	<section class="access">
+		<div class="image"></div>
+		<h1></h1>
+	</section>
+	<section class="shows">
+	<video width="540" height="360" controls>
+		<source src="www.youtube.com/watch?v=DN-1A02X2sI" type="video/flv">
+	</video>
+	<video width="540" height="360" controls>
+		<source src="www.youtube.com/watch?v=DN-1A02X2sI" type="video/flv">
+	</video>
+	</section>
+	<section class="photos">Photos</section>
 
 	<footer>Rodapé
 		<script type="text/javascript" src=""></script>
