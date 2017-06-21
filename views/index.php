8e4f0cf9ac738d431913107a63f2f33573ce312d
KaioPorto <kaio.rodrigo28@gmail.com>
2017-06-21 17:35:58 -0300

    Changing tabs to spaces

--- views/index.php
+++ views/index.php
@@ -2,44 +2,44 @@
 <html>
 <body>
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="description" content="Acesso Restrito é uma banda do">
-	<meta name="viewport" content="width=device-width, intial-scale=1, user-scalable=no">
-	<meta name="author" content="Kaio Rodrigo S. Porto">
-	<title>Acesso Restrito</title>
-	<link rel="stylesheet" type="text/css" href="../bower_components/gaintime/css/gaintime.min.css">
-	<link rel="stylesheet" type="text/css" href="../assets/main.css">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Acesso Restrito é uma banda do">
+    <meta name="viewport" content="width=device-width, intial-scale=1, user-scalable=no">
+    <meta name="author" content="Kaio Rodrigo S. Porto">
+    <title>Acesso Restrito</title>
+    <link rel="stylesheet" type="text/css" href="../bower_components/gaintime/css/gaintime.min.css">
+    <link rel="stylesheet" type="text/css" href="../assets/main.css">
 </head>
-	<header class="gt-top-menu xs-left">
-		<img src="">
-		<label class="menu-toggle"></label>
-		<nav>
-			<ul>
-				<a><li>Home</li></a>
-				<a><li>Photos</li></a>
-				<a><li>Contato</li></a>
-				<a><li>Agenda</li></a>
-			</ul>
-		</nav>
-	</header>
+    <header class="gt-top-menu xs-left">
+        <img src="">
+        <label class="menu-toggle"></label>
+        <nav>
+            <ul>
+                <a><li>Home</li></a>
+                <a><li>Photos</li></a>
+                <a><li>Contato</li></a>
+                <a><li>Agenda</li></a>
+            </ul>
+        </nav>
+    </header>
 
-	<section class="access">
-		<div class="image"></div>
-		<h1></h1>
-	</section>
-	<section class="shows">
-	<video width="540" height="360" controls>
-		<source src="www.youtube.com/watch?v=DN-1A02X2sI" type="video/flv">
-	</video>
-	<video width="540" height="360" controls>
-		<source src="www.youtube.com/watch?v=DN-1A02X2sI" type="video/flv">
-	</video>
-	</section>
-	<section class="photos">Photos</section>
+    <section class="access">
+        <div class="image"></div>
+        <h1></h1>
+    </section>
+    <section class="shows">
+    <video width="540" height="360" controls>
+        <source src="www.youtube.com/watch?v=DN-1A02X2sI" type="video/flv">
+    </video>
+    <video width="540" height="360" controls>
+        <source src="www.youtube.com/watch?v=DN-1A02X2sI" type="video/flv">
+    </video>
+    </section>
+    <section class="photos">Photos</section>
 
-	<footer>Rodapé
-		<script type="text/javascript" src=""></script>
-	</footer>
+    <footer>Rodapé
+        <script type="text/javascript" src=""></script>
+    </footer>
 </body>
 </html>
